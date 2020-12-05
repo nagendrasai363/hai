6b497b1649f7f657a56f1cc2151b57894da972fc
Sai Nagendra <51019078+nagendrasai363@users.noreply.github.com>
2020-12-05 13:29:15 +0530

    Update Docker-script.sh

--- docker/Files/Docker-script.sh
+++ docker/Files/Docker-script.sh
@@ -4,4 +4,6 @@ chown -R www-data /Sites/online_test/yaksh
 chown -R nobody /Sites/online_test/yaksh_data
 chmod -R 664 /Sites/online_test
 chmod -R +X /Sites
+python3 manage.py makemigrations
+python3 manage.py makemigrations notifications_plugin
 /usr/sbin/apache2ctl -D FOREGROUND
